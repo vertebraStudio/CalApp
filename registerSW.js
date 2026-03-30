193cc85e66eef499fdbb57880139a24d6ce6bce8
vertebraStudio <dortizmesa@gmail.com>
2026-03-30 14:17:38 +0000

    Deploying to gh-pages from @ vertebraStudio/CalApp@40152ae3948e39bc3beca2021699248383faf431 🚀

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/CalApp/sw.js', { scope: '/CalApp/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
